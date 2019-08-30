2e89f01d43b95d8ff806bc3ef38e10e748ae01a1
Yarko Tymciurak <yarkot1@gmail.com>
2019-08-30 07:36:00 -0500

    document hard reset for master

--- merge_master.sh
+++ merge_master.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # git checkout --patch <from-branch> <filename> ...
 # See:  https://git-scm.com/docs/git-checkout#Documentation/git-checkout.txt---patch
+
+# to pull origin/master w/ certainty:
+# (ref: https://stackoverflow.com/questions/9589814/how-do-i-force-git-pull-to-overwrite-everything-on-every-pull/9589927)
+git fetch origin master
+git reset --hard FETCH_HEAD
+# this will remove ignored directories too:
+git clean -df
 #
 # if the file does not exist in your current (target) branch,
 # then just do a checkout
